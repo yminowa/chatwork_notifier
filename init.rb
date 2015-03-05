c151d523f4b01ef4988ddcf99ff90faaac67b393
Yuichi Minowa(@y_minowa) <yuichi.minowa@u-cast.com>
2015-03-05 17:51:37 +0900

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :chatwork_notifier do
+  name 'Chatwork Notifier plugin'
+  author 'Yuichi MINOWA (@y_minowa)'
+  description 'Notify ChatWork Rooms of Redmine info.'
+  version '0.0.1'
+  url 'https://github.com/yminowa/chatwork_notifier'
+  author_url 'http://blog.mino3.com/'
+end
